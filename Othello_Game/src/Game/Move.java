7fcebdd001b420a042ebafbf02b1a7be207d30c8
Shiva <shivaasus531@gmail.com>
2022-12-19 13:51:54 +0530

    makeMove added to Move class

--- Othello_Game/src/Game/Move.java
+++ Othello_Game/src/Game/Move.java
@@ -5,4 +5,31 @@ public class Moves extends Board{
 		super(boardSize, ch1, ch2);
 		// TODO Auto-generated constructor stub
 	}
+
+	public void makeMove(Integer x, Integer y, Character ch) {to make a move at x, y point if a move can be made there
+		
+		Integer arr[][]= {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};		
+		
+		getBoard()[x][y]=ch;
+		for(Integer i=0;i8;i++) {				
+			Integer X=x+arr[i][0];
+			Integer Y=y+arr[i][1];	
+			Integer count=0;
+			while(inRange(X, Y) && getBoard()[X][Y]!='0' && getBoard()[X][Y]!=ch) {
+				X+=arr[i][0];
+				Y+=arr[i][1];
+				count++;
+			}
+			if(inRange(X, Y) && getBoard()[X][Y]==ch && count=1) {					
+				X=x+arr[i][0];
+				Y=y+arr[i][1];
+				while(inRange(X, Y) && getBoard()[x][y]!='0' && getBoard()[X][Y]!=ch) {
+					getBoard()[X][Y]=ch;
+					X+=arr[i][0];
+					Y+=arr[i][1];
+				}
+			}
+		}			
+				
+	}
 }
