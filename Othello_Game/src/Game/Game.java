4552c6e83c418cba07f59c1f4ffb8e4a4e1d259a
b19621 <saishsawant25102001@gmail.com>
2022-12-19 19:46:06 +0530

    Changes made in game class

--- Othello_Game/src/Game/Game.java
+++ Othello_Game/src/Game/Game.java
@@ -39,7 +39,50 @@ public void startGame() {
 					else {
 						System.out.println("Enter player1("+player1.getPieces()+")'s move : ");
 					}
+				}else {
+					if(move.noMovesAvailable(player2.getPieces())) {
+						System.out.println("Player2 ("+player2.getPieces()+") cannot make a move");
+						player1Turn=true;
+						continue;
+					}
+					else {
+						System.out.println("Enter player2 ("+player2.getPieces()+")'s move : ");
+					}
 				}
+				while(!move.inRange(x, y) || !(move.isAvailable(x, y))) {
+					System.out.println("Enter a Valid Point");
+					x=s2.nextInt();
+					y=s2.nextInt();
+					
+				}				
+				
+				if(player1Turn) {										
+					while(!move.canMove(x, y, player1.getPieces())) {
+						System.out.println("Enter a point where a move can be made : ");					
+						x=s2.nextInt();
+						y=s2.nextInt();
+					}					
+				}
+				else {									
+					while(!move.canMove(x, y, player2.getPieces())) {								
+						System.out.println("Enter a point where a move can be made : ");					
+						x=s2.nextInt();
+						y=s2.nextInt();
+					}							
+				}
+				
+				if(player1Turn) {
+					move.makeMove(x, y, player1.getPieces());
+					player1Turn=false;
+				}
+				else {
+					move.makeMove(x, y, player2.getPieces());
+					player1Turn=true;
+				}
+				
+				move.print();
+				
+			}			
 }
 
 private Player input(Integer num) {
