56c0d5898b4db89bc6b55d104d98d14341209537
b19621 <saishsawant25102001@gmail.com>
2022-12-19 13:39:50 +0530

    created constructor

--- Othello_Game/src/Game/Move.java
+++ Othello_Game/src/Game/Move.java
@@ -1,5 +1,8 @@
 package Game;
 
 public class Moves extends Board{
-
+    public Move(Integer boardSize, Character ch1, Character ch2) {
+		super(boardSize, ch1, ch2);
+		// TODO Auto-generated constructor stub
+	}
 }
