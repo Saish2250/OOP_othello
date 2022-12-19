b68cbdab0d04f5470423041eed9d1fc6083b981b
joel <joel80851@gmail.com>
2022-12-19 21:12:21 +0530

    getWinner fixed

--- Othello_Game/src/Game/Board.java
+++ Othello_Game/src/Game/Board.java
@@ -18,6 +18,34 @@ package othello;
 			board[index+1][index]=ch1;
 					
 		}
+
+		public Character getWinner(Character ch1, Character ch2) {
+				
+		    Integer count1=0;
+		    Integer count2=0;
+		    for(Integer i=0;i<getBoardSize();i++) {
+		        for(Integer j=0;j<getBoardSize();j++) {
+		            if(getBoard()[i][j]==ch1) {
+		                count1++;
+		            }
+		        }
+		    }
+		    count2=((getBoardSize()*getBoardSize())-count1);
+    
+		    System.out.println("player1("+ch1+")'s score is : "+count1);
+		    System.out.println("player2("+ch2+")'s score is : "+count2);
+    
+		    if(count1>count2) {
+		        return ch1;
+		    }
+		    else if(count1<count2) {
+		        return ch2;
+		    }
+		    else {
+		        return '\0';
+		    }
+	    
+		}
 		
 		
 
