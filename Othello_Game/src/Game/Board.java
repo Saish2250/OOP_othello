b059d701bee5358cc1059a1a4e17cb61f560426b
b19621 <saishsawant25102001@gmail.com>
2022-12-19 15:26:18 +0530

    Board class updated

--- Othello_Game/src/Game/Board.java
+++ Othello_Game/src/Game/Board.java
@@ -1,20 +1,45 @@
-package Game;
-
-public class Board extends Game{
-	private Character board[][];
-		private Integer boardSize;	
-
+package othello;
+	public class Board{
+		
+		
+		private char board[][];
+		private Integer boardSize;
+		
 		public Board(Integer boardSize, Character ch1, Character ch2) {
-
+			
 			this.boardSize=boardSize;
-
-			board=new Character[boardSize][boardSize];
-
+			
+			board=new char[boardSize][boardSize];
+			
 			Integer index=(boardSize-1)/2;
 			board[index][index]=ch2;
 			board[index+1][index+1]=ch2;
 			board[index][index+1]=ch1;
 			board[index+1][index]=ch1;
+					
+		}
+		
+		
 
+		
+			
+			
+			public char[][] getBoard() {
+				return board;
+			}
+
+			public void setBoard(char board[][]) {
+				this.board = board;
+			}
+
+			public int getBoardSize() {
+				return boardSize;
+			}
+
+			public void setBoardSize(int boardSize) {
+				this.boardSize = boardSize;
+			}
+			
+
+	
 	}
-}
