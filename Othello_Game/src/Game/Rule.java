5b9e883b1e7b13a079514d7e28762c09e8c4435f
imatationcode <shivaasus531@gmail.com>
2022-12-21 20:41:47 +0530

    Rule class added

---
+++ Othello_Game/src/Game/Rule.java
@@ -0,0 +1,5 @@
+package othello;
+public class Rule {
+    System.out.println(" \n1. The game will ask for player 1's name\n2. Then game will ask for player 1's sign(usually B for Black )\n3. Then game will ask for player 2's name\n4. The game will ask for player 2's sign(usually W for white )\n5. Now just keep on providing x and y coordinates of the board, where you want to mark your sign, game will check if it is valid move or not.\n6. The made move will be valid only if the sign makes an Sandwich , \nwhere the player who's turn is considred as bread and the opponenets sign will be considered as stuff in it E.g -W B W\n7.There is no limit for the number of signs in between the bread \n8. If one player is not able to make a move he can just pass it to different player.\n");
+
+}
