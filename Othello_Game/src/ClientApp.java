053a33c40075d878e27899ad9e86618d0be9a3ce
b19621 <saishsawant25102001@gmail.com>
2022-12-23 01:13:23 +0530

    Added menu to clientapp

--- Othello_Game/src/ClientApp.java
+++ Othello_Game/src/ClientApp.java
@@ -1,11 +1,49 @@
 import Game.Game;
+import Game.Rule;
+import java.util.*;
 
 public class ClientApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Game g1= new Game();
-		g1.startGame();
-	}
-
+		public static void main(String[] args) {
+			int choice;
+			// TODO Auto-generated method stub 
+		        Scanner input = new Scanner(System.in);
+		        do {
+		        System.out.println("Welcome to the Othello game! Would you like to play or read the rules?");
+		        System.out.println("1. Play");
+		        System.out.println("2. Read rules");
+		        System.out.println("0. Exit");
+		        while(true) {
+		    		try {
+		    			choice = input.nextInt();
+		    			break;
+		    		} catch (Exception e) {
+		    			// TODO: handle exception
+		    			System.out.println("Invalid value re-enter");
+		    			input.next();
+		    		}
+		    	}
+		        
+		        switch (choice) {
+		            case 1:
+		                Game g1 = new Game();
+		                g1.startGame();
+		                break;
+		            case 2:{
+		            	Rule r1 = new Rule();
+		                r1.readRules();
+		                break;	
+		            }
+		                
+		            default:
+		            	if(choice!=0) {
+		            	System.out.println("If you want to exit enter 0");
+		                break;
+		            	}
+		        	}
+		        }while(choice!=0);
+		        System.out.println("Exited Successfully");
+		 }
 }
+
+
--- Othello_Game/src/Game/Rule.java
+++ Othello_Game/src/Game/Rule.java
@@ -1,6 +1,6 @@
 package Game;
 public class Rule {
 	public void readRules() {
-    System.out.println(" \n1. The game will ask for player 1's & 2's name\n2. Then game will assign player 1 with white piece and player 2 with black piece\n3. Then game will ask for player 2's name\n2. Now just keep on providing x and y coordinates of the board, where you want to mark your sign, game will check if it is valid move or not.\n3. The made move will be valid only if the sign makes an Sandwich , \nwhere the player who's turn is considred as bread and the opponenets sign will be considered as stuff in it E.g -W B W\n4.There is no limit for the number of signs in between the bread \n5. If one player is not able to make a move he can just pass it to different player.\n");
+	    System.out.println(" \n1. The game will ask for player 1's & 2's name\n2. Then game will assign player 1 with white piece and player 2 with black piece\n3. Then game will ask for player 2's name\n2. Now just keep on providing x and y coordinates of the board, where you want to mark your sign, game will check if it is valid move or not.\n3. The made move will be valid only if the sign makes an Sandwich , \nwhere the player who's turn is considred as bread and the opponenets sign will be considered as stuff in it E.g -W B W\n4.There is no limit for the number of signs in between the bread \n5. If one player is not able to make a move he can just pass it to different player.\n");
 	}
 }
